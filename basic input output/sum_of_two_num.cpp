b879929807bb26f5f8a90df78558e49bd4fd8023
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-11-16 22:48:04 +0530

    sum of two num

---
+++ basic input output/sum_of_two_num.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// basic sum program for two numbers by taking input from user 
+int main(){
+    int a,b;//defining data type of variables  
+    cout<<"Enter 1st number :";
+    cin>>a;
+    cout<<"Enter 2nd number :";
+    cin>>b;
+    int sum = a+b;
+    cout<<"the sum of "<<a<<" and "<< b<<" is "<<sum<<endl;
+    return 0;
+}
