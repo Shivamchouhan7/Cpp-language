abf3f91fb742408b5dae27c93b56706e0e043197
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-11-23 23:37:55 +0530

    Inverted Triangle pattern

---
+++ Patterns/Inverted_triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/*Inverted Triangle pattern like
+****
+ ***
+  **
+   * 
+      */
+int main(){
+    int n;
+    cout << "Enter size (line) for triangle pattern : ";//enter 4 for above example triangle 
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {for(int j=0;j<i;j++){
+        cout<<" ";
+    }
+    for(int k=0;k<n-i;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+        
+    }
+    
+
+    return 0;
+}
