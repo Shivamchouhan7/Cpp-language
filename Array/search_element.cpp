d7b7c8c7528134e6b3cc1e0909c96a1137440430
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-12-08 20:23:28 +0530

    Searching element in array

---
+++ Array/search_element.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// Searching element in array( print -1 if not found)
+int main(){
+    int array[6]={1,2,3,7,8,9};
+    int element,flag=-1;
+    cout<<"Enter element you want to search :";
+    cin>>element;
+
+    for(int i=0;i<6;i++){
+        if(element==array[i]){
+            flag=1;
+            cout<<"Element found at index "<<i<<endl;
+        }
+    }
+    cout<<flag;
+    return 0;
+}
